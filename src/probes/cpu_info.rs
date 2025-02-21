c2a4620991b989b430a48a5a073ca65aaeea2862
victoryeagle77 <victoryeagle77@protonmail.com>
2025-02-21 15:25:52 +0100

    Add files via upload

---
+++ src/probes/cpu_info.rs
@@ -0,0 +1,252 @@
+//! # CPU data Module
+//!
+//! This module provides functionality to retrieve processor data on Unix-based systems.
+
+use log::error;
+use serde::Serialize;
+use serde_json::json;
+use std::{fs, thread::sleep, time::Duration, time::Instant};
+use sysinfo::{CpuExt, System, SystemExt};
+
+use crate::utils::{parse_file_content, read_file_content, write_json_to_file};
+
+const CPUINFO: &str = "/proc/cpuinfo";
+const THERMAL: &str = "/sys/class/thermal";
+const RAPL: &str = "/sys/class/powercap";
+
+const HEADER: &str = "CPU";
+const LOGGER: &str = "log/cpu_data.json";
+
+/// Collection of collected CPU data
+#[derive(Debug, Serialize)]
+struct CpuInfo {
+    /// Model name.
+    model: String,
+    /// Vendor ID.
+    id: String,
+    /// CPU generation.
+    family: String,
+    /// CPU microcode revision.
+    rev: String,
+    /// CPU operating frequency in Mhz.
+    freq: String,
+    /// CPU memory cache size.
+    cache: String,
+    /// CPU address sizes available.
+    addr: String,
+    /// Physical CPU cores.
+    phy_cores: usize,
+    /// Logical CPU cores.
+    lgc_cores: usize,
+    /// CPU usage cores in percentage.
+    use_cores: Vec<f32>,
+    /// CPU temperatures by zone in °C.
+    temp: Vec<(String, f32)>,
+    /// CPU energy consumption by zone in uJ.
+    pwr: Vec<(String, f64)>,
+}
+
+/// Retrieves the current CPU usage for all cores.
+/// This function uses the `sysinfo` crate to gather CPU usage information.
+/// It takes two snapshots of CPU usage with a 1-second interval between them,
+/// to calculate the current usage percentage for each CPU core.
+///
+/// # Return
+///
+/// A vector where each element represents the usage percentage of a CPU core.
+/// The order of the elements corresponds to the order of the CPU cores as reported by the system.
+/// If resources are not accessible, return 0.0 as value and log the error.
+///
+/// # Performance considerations
+///
+/// This function introduces a 1-second delay due to the sleep between CPU usage snapshots.
+/// This delay is necessary to calculate an accurate usage percentage.
+fn get_cpu_usage() -> Vec<f32> {
+    let mut sys: System = System::new_all();
+
+    sys.refresh_cpu();
+    sleep(Duration::from_secs(1));
+    sys.refresh_cpu();
+
+    let cpus: &[sysinfo::Cpu] = sys.cpus();
+
+    if cpus.is_empty() {
+        error!("[CPU_CORE_USAGE] 'Unable to get CPU core information'");
+        return vec![0.0];
+    }
+
+    cpus.iter()
+        .enumerate()
+        .map(|(index, cpu)| {
+            let usage = cpu.cpu_usage();
+            if usage.is_nan() || usage.is_infinite() {
+                error!(
+                    "[CPU_CORE_USAGE] 'Unable to get valid CPU usage for core {}'",
+                    index
+                );
+                0.0
+            } else {
+                usage
+            }
+        })
+        .collect()
+}
+
+/// Retrieves and displays CPU temperature information from the system.
+/// This function scans the thermal zones in the system (typically located in `/sys/class/thermal`)
+/// and attempts to read and display the temperature for each zone that starts with "thermal_zone".
+///
+/// # Return
+///
+/// - `result` : Vector where each element represents cores and its thermal state in Celsius.
+/// - An error message if no thermal files or data are found.
+fn get_cpu_temp() -> Vec<(String, f32)> {
+    let mut result = Vec::new();
+    let mut found: bool = false;
+
+    if let Ok(entries) = fs::read_dir(THERMAL) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if !path.is_dir()
+                || !path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .starts_with("thermal_zone")
+            {
+                continue;
+            }
+
+            let type_content = match read_file_content(path.join("type").to_str().unwrap()) {
+                Some(content) => content,
+                None => continue,
+            };
+
+            let temp_content = match read_file_content(path.join("temp").to_str().unwrap()) {
+                Some(content) => content,
+                None => continue,
+            };
+
+            if let Ok(temp) = temp_content.trim().parse::<i32>() {
+                let temperature = temp as f32 / 1000.0;
+                result.push((type_content.trim().to_string(), temperature));
+                found = true;
+            }
+        }
+    } else {
+        error!("[CPU_TEMPERATURE] File '{}'", THERMAL);
+    }
+
+    if !found || result.is_empty() {
+        result.push(("NULL".to_string(), 0.0));
+        error!("[CPU_TEMPERATURE] Data 'Unable to get valid CPU temperature information'");
+    }
+
+    result
+}
+
+/// Function reading in RAPL directory : `/sys/class/powercap/`,
+/// to get consumption data in locate each CPU zone to get specific energy consumption.
+///
+/// # Return
+///
+/// - `result` : Vector containing CPU zone name and its consumption.
+/// - An error message if no energy consumption file or data are found.
+fn get_cpu_consumption() -> Vec<(String, f64)> {
+    let mut result = Vec::new();
+    let start_time = Instant::now();
+
+    if let Ok(entries) = fs::read_dir(RAPL) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_dir() {
+                let domain = path.file_name().unwrap().to_str().unwrap();
+                if domain.starts_with("intel-rapl:") {
+                    if let Some(start_energy) =
+                        read_file_content(path.join("energy_uj").to_str().unwrap())
+                            .and_then(|content| content.trim().parse::<f64>().ok())
+                    {
+                        sleep(Duration::from_secs(1));
+                        if let Some(end_energy) =
+                            read_file_content(path.join("energy_uj").to_str().unwrap())
+                                .and_then(|content| content.trim().parse::<f64>().ok())
+                        {
+                            let elapsed = start_time.elapsed().as_secs_f64();
+                            let power = (end_energy - start_energy) / (elapsed * 1_000_000.0);
+                            result.push((domain.to_string(), power));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if result.is_empty() {
+        result.push(("NULL".to_string(), 0.0));
+        error!(
+            "[CPU_POWER_CONSUMPTION] Data 'Unable to get valid CPU power consumption information'"
+        );
+    }
+
+    result
+}
+
+/// Public function reading and using `/proc/cpuinfo` file values,
+/// and retrieves detailed CPU data.
+///
+/// # Return
+///
+/// `result` : Completed `CpuInfo` structure with all cpu information
+/// - CPU full model name
+/// - CPU maker identification
+/// - CPU general generation
+/// - CPU family specific model number
+/// - CPU revision number that indicates minor changes or corrections to the model
+/// - CPU operating frequency
+/// - CPU cache size which is fast memory built into the processor to speed up operations
+/// - CPU address sizes
+/// - CPU physical cores that are the actual processing units on the chip
+/// - CPU ID level which is the maximum option set that you can safely use to interrogate the processor for information
+/// - CPU logical cores that includes physical and virtual cores
+/// - CPU detailed core usage
+fn collect_cpu_data() -> Result<CpuInfo, String> {
+    let cpu_usage = get_cpu_usage();
+    let cpu_temps = get_cpu_temp();
+    let cpu_pwr = get_cpu_consumption();
+    let cpu_value = |key: &str| {
+        parse_file_content(CPUINFO, ":")
+            .iter()
+            .find(|(k, _)| k == key)
+            .map(|(_, v)| v.to_string())
+            .unwrap_or_else(|| "NULL".to_string())
+    };
+
+    let result: CpuInfo = CpuInfo {
+        model: cpu_value("model name"),
+        id: cpu_value("vendor_id"),
+        family: cpu_value("cpu family"),
+        rev: cpu_value("stepping"),
+        freq: cpu_value("cpu MHz"),
+        cache: cpu_value("cache size"),
+        addr: cpu_value("address sizes"),
+        phy_cores: num_cpus::get_physical(),
+        lgc_cores: num_cpus::get(),
+        use_cores: cpu_usage,
+        temp: cpu_temps,
+        pwr: cpu_pwr,
+    };
+
+    Ok(result)
+}
+
+/// Public function used to send JSON formatted values,
+/// from `collect_cpu_data` function result.
+pub fn get_cpu_info() {
+    let data = || {
+        let values = collect_cpu_data()?;
+        Ok(json!({ HEADER: values }))
+    };
+
+    write_json_to_file(data, LOGGER, HEADER);
+}
---
+++ src/probes/load_info.rs
@@ -0,0 +1,184 @@
+//! # System Load data Module
+//!
+//! This module provides functionality to retrieve system load data on Unix-based systems.
+
+use chrono::{SecondsFormat::Millis, Utc};
+use log::error;
+use serde::Serialize;
+use serde_json::json;
+use std::time::Duration;
+use sysinfo::{PidExt, ProcessExt, System, SystemExt};
+
+use crate::utils::{read_file_content, write_json_to_file};
+
+const UPTIME: &str = "/proc/uptime";
+const LOADAVG: &str = "/proc/loadavg";
+
+const HEADER: &str = "LOAD_SYSTEM";
+const LOGGER: &str = "log/load_data.json";
+
+/// Collection of collected system load data
+#[derive(Debug, Serialize)]
+struct SystemLoad {
+    /// Time since the last system boot.
+    uptime: Option<(u64, u64, u64)>,
+    /// System load after 1 minute.
+    load_1: Option<f64>,
+    /// System load after 5 minutes.
+    load_5: Option<f64>,
+    /// System load after 15 minutes.
+    load_15: Option<f64>,
+    /// Total of running processes.
+    run_proc: Option<u32>,
+    /// Total processes counted.
+    tot_proc: Option<u32>,
+    /// PID of the top resource-consuming process.
+    top_process_pid: Option<u32>,
+    /// Name of the top resource-consuming process.
+    top_process_name: Option<String>,
+    /// CPU usage of the top resource-consuming process in Bytes.
+    top_process_cpu_usage: Option<f32>,
+    /// Memory usage of the top resource-consuming process in percentage.
+    top_process_memory_usage: Option<u64>,
+}
+
+/// Function that retrieves the system uptime data from `/proc/uptime`.
+///
+/// # Returns
+///
+/// - Option containing a tuple of days, hours, and minutes since last boot
+fn get_uptime() -> Option<(u64, u64, u64)> {
+    let content = read_file_content(UPTIME)?;
+
+    let uptime_secs = content
+        .split_whitespace()
+        .next()
+        .and_then(|s| s.parse::<f64>().ok())?;
+
+    let uptime = Duration::from_secs_f64(uptime_secs);
+
+    let days = uptime.as_secs() / 86400;
+    let hours = (uptime.as_secs() % 86400) / 3600;
+    let minutes = (uptime.as_secs() % 3600) / 60;
+
+    Some((days, hours, minutes))
+}
+
+/// Function that retrieves information about the top resource-consuming process.
+///
+/// # Returns
+///
+/// A tuple containing the following information about the top process :
+/// - PID
+/// - Process name
+/// - CPU usage
+/// - Memory usage
+fn get_top_process() -> (Option<u32>, Option<String>, Option<f32>, Option<u64>) {
+    let mut system: System = System::new_all();
+    system.refresh_all();
+
+    let mut top_process = None;
+    let mut max_usage: f32 = 0.0;
+
+    for (pid, process) in system.processes() {
+        let cpu_usage = process.cpu_usage();
+        if cpu_usage > max_usage {
+            max_usage = cpu_usage;
+            top_process = Some((
+                pid.as_u32(),
+                process.name().to_string(),
+                cpu_usage,
+                process.memory() / 1_000,
+            ));
+        }
+    }
+
+    top_process.map_or((None, None, None, None), |(pid, name, cpu, mem)| {
+        (Some(pid), Some(name), Some(cpu), Some(mem))
+    })
+}
+
+/// Function that retrieves detailed system load data.
+/// Reads `/proc/loadavg` to get load averages and process data.
+///
+/// # Returns
+///
+/// `Result<SystemLoad, String>` : Completed `SystemLoad` structure with all load system information
+/// or an error message if data collection fails.
+fn collect_load_data() -> Result<SystemLoad, String> {
+    let content = read_file_content(LOADAVG)
+        .ok_or_else(|| "File 'Unable to read load average file'".to_string())?;
+
+    let line = content.trim();
+    let parts: Vec<&str> = line.split_whitespace().collect();
+
+    if parts.len() < 5 {
+        error!("[{HEADER}] Data 'Insufficient data in {LOADAVG}'");
+        return Err("Insufficient data".to_string());
+    }
+
+    let run_proc: Vec<&str> = parts[3].split('/').collect();
+    let (running, total) = if run_proc.len() == 2 {
+        (
+            run_proc[0].parse::<u32>().ok(),
+            run_proc[1].parse::<u32>().ok(),
+        )
+    } else {
+        error!("[{HEADER}] Data 'Failed data extraction from {LOADAVG}'",);
+        (None, None)
+    };
+
+    let (top_process_pid, top_process_name, top_process_cpu_usage, top_process_memory_usage) =
+        get_top_process();
+
+    let result = SystemLoad {
+        uptime: get_uptime(),
+        load_1: parts[0].parse::<f64>().ok(),
+        load_5: parts[1].parse::<f64>().ok(),
+        load_15: parts[2].parse::<f64>().ok(),
+        run_proc: running,
+        tot_proc: total,
+        top_process_pid,
+        top_process_name,
+        top_process_cpu_usage,
+        top_process_memory_usage,
+    };
+
+    Ok(result)
+}
+
+/// Public function used to send JSON formatted values,
+/// from `collect_load_data` function result.
+pub fn get_load_info() {
+    let data = || {
+        let values = collect_load_data()?;
+        let timestamp = Utc::now().to_rfc3339_opts(Millis, true);
+        Ok(json!({
+            HEADER: {
+                "timestamp": timestamp,
+                "uptime": values.uptime.map(|(days, hours, minutes)| {
+                    json!({
+                        "days": days,
+                        "hours": hours,
+                        "minutes": minutes
+                    })
+                }),
+                "system_load": {
+                    "1_min": values.load_1,
+                    "5_min": values.load_5,
+                    "15_min": values.load_15
+                },
+                "running_process": values.run_proc,
+                "total_process": values.tot_proc,
+                "top_process": {
+                    "pid": values.top_process_pid,
+                    "name": values.top_process_name,
+                    "cpu_usage": values.top_process_cpu_usage,
+                    "memory_usage": values.top_process_memory_usage,
+                },
+            }
+        }))
+    };
+
+    write_json_to_file(data, LOGGER, HEADER);
+}
