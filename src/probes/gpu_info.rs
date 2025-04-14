20475df820ebcbb11c926545fd8f9b32b7f415b1
victoryeagle77 <victoryeagle77@protonmail.com>
2025-04-15 00:04:42 +0200

    Update gpu_info.rs

--- src/probes/gpu_info.rs
+++ src/probes/gpu_info.rs
@@ -4,10 +4,10 @@
 
 use log::error;
 use nvml_wrapper::{
-    enum_wrappers::device::TemperatureSensor,
+    enum_wrappers::device::{Clock, ClockId, TemperatureSensor},
     enums::device::DeviceArchitecture,
     error::NvmlError,
-    struct_wrappers::device::{MemoryInfo, ProcessUtilizationSample, Utilization},
+    struct_wrappers::device::{MemoryInfo, PciInfo, ProcessUtilizationSample, Utilization},
     Nvml,
 };
 use serde::Serialize;
@@ -20,12 +20,20 @@ const HEADER: &str = "GPU";
 const LOGGER: &str = "log/gpu_data.json";
 
 /// Collection of collected GPU data
-#[derive(Debug, Serialize)]
+#[derive(Serialize)]
 struct GpuInfo {
     /// GPU architecture.
     gpu_arch: Option<String>,
     /// GPU PCIe bus identification.
     gpu_bus_id: Option<String>,
+    /// GPU graphic clock usage in MHz.
+    gpu_clock_graphic: Option<u32>,
+    /// GPU memory clock usage in MHz.
+    gpu_clock_memory: Option<u32>,
+    /// GPU streaming multiprocessor clock usage in MHz.
+    gpu_clock_sm: Option<u32>,
+    /// GPU video clock usage in MHz.
+    gpu_clock_video: Option<u32>,
     /// GPU energy consumption in J.
     gpu_energy_consumption: Option<u64>,
     /// Speed per fan in percentage.
@@ -46,8 +54,8 @@ struct GpuInfo {
     gpu_memory_usage: Option<f32>,
     /// GPU electrical consumption in W.
     gpu_power_consumption: Option<u32>,
-    /// GPU power limit in W.
-    gpu_power_limit: Option<u32>,
+    /// GPU power ratio in percentage.
+    gpu_power_usage: Option<f32>,
     /// Process decoder utilization in percentage.
     process_dec: Option<u32>,
     /// Process encoder utilization in percentage.
@@ -66,15 +74,19 @@ impl GpuInfo {
         json!({
             "gpu_architecture": self.gpu_arch.as_deref().map(Some).unwrap_or(None),
             "gpu_bus_id": self.gpu_bus_id.as_deref().map(Some).unwrap_or(None),
+            "gpu_clock_graphic_MHz": self.gpu_clock_graphic.map(Some).unwrap_or(None),
+            "gpu_clock_memory_MHz": self.gpu_clock_memory.map(Some).unwrap_or(None),
+            "gpu_clock_sm_MHz": self.gpu_clock_sm.map(Some).unwrap_or(None),
+            "gpu_clock_video_MHz": self.gpu_clock_video.map(Some).unwrap_or(None),
             "gpu_energy_consumption_J": self.gpu_energy_consumption.unwrap_or(0) / 1_000,
             "gpu_fan_speeds_%": self.gpu_fan_speed.iter().map(|&speed| speed.unwrap_or(0)).collect::<Vec<u32>>(),
             "gpu_power_consumption_W": self.gpu_power_consumption.unwrap_or(0) / 1_000,
-            "gpu_power_limit_W": self.gpu_power_limit.unwrap_or(0) / 1_000,
+            "gpu_power_usage_%": self.gpu_power_usage.unwrap_or(0.0),
             "gpu_temperature_°C": self.gpu_temperature.map(Some).unwrap_or(None),
             "gpu_memory_free_GB": self.gpu_memory_free.map(Some).unwrap_or(None),
-            "gpu_memory_stat_%": self.gpu_memory_stat.map(Some).unwrap_or(None),
             "gpu_memory_total_GB": self.gpu_memory_total.map(Some).unwrap_or(None),
             "gpu_memory_usage_GB": self.gpu_memory_usage.map(Some).unwrap_or(None),
+            "gpu_memory_usage_%": self.gpu_memory_stat.map(Some).unwrap_or(None),
             "gpu_name": self.gpu_name.as_deref().map(Some).unwrap_or(None),
             "gpu_usage_%": self.gpu_usage.map(Some).unwrap_or(None),
             "process_decoder_%": self.process_dec.map(Some).unwrap_or(None),
@@ -90,17 +102,7 @@ impl GpuInfo {
 ///
 /// # Return
 ///
-/// `GpuInfo` : Completed `GpuInfo` structure with all GPU information:
-/// - Number of GPUs.
-/// - Name of GPU model.
-/// - Power consumption.
-/// - Total computing memory.
-/// - Used computing memory.
-/// - Free computing memory.
-/// - GPU usage in percentage.
-/// - GPU computing memory usage in percentage.
-/// - Speed per fan for graphics card in percentage.
-/// - Streaming Multiprocessor, memory, encoder, and decoder utilization in percentage.
+/// - Completed [`GpuInfo`] structure with all GPU information
 fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
     let nvml: Nvml = Nvml::init().map_err(|e: NvmlError| {
         error!("[{HEADER}] Library 'Failed to initialize NVML' : {e}");
@@ -112,11 +114,6 @@ fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
         e
     })?;
 
-    if gpus == 0 {
-        error!("[{HEADER}] Data 'No GPUs detected'");
-        return Ok(Vec::new());
-    }
-
     let mut result: Vec<GpuInfo> = Vec::new();
 
     for index in 0..gpus {
@@ -126,38 +123,83 @@ fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
                 e
             })?;
 
+        let power = device
+            .power_usage()
+            .map_err(|e: NvmlError| {
+                error!("[{HEADER}] Data 'Failed to get power consumption' : {e}")
+            })
+            .ok();
+        let limit = device
+            .power_management_limit()
+            .map_err(|e: NvmlError| {
+                error!("[{HEADER}] Data 'Failed to get power consumption' : {e}")
+            })
+            .ok();
+
         let mut data: GpuInfo = GpuInfo {
-            gpu_bus_id: device.pci_info().ok().map(|pci| pci.bus_id.clone()),
-            gpu_arch: device
-                .architecture()
+            gpu_bus_id: device.pci_info()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to GPU PCI bus identification' : {e}"))
                 .ok()
-                .map(|a: DeviceArchitecture| format!("{:?}", a)),
-            gpu_name: device.name().ok(),
-            gpu_usage: device.utilization_rates().ok().map(|u: Utilization| u.gpu),
+                .map(|pci: PciInfo| pci.bus_id.clone()),
 
-            gpu_fan_speed: (0..device.num_fans().unwrap_or(0))
-                .map(|i: u32| device.fan_speed(i).ok())
-                .collect(),
-            gpu_temperature: device.temperature(TemperatureSensor::Gpu).ok(),
+            gpu_arch: device.architecture()
+                .map_err(|e: NvmlError| {
+                    error!("[{HEADER}] Data 'Failed to get architecture type' : {e}")
+                })
+                .ok()
+                .map(|a: DeviceArchitecture| format!("{a:?}")),
 
-            gpu_energy_consumption: device.total_energy_consumption().ok(),
-            gpu_power_consumption: device.power_usage().ok(),
-            gpu_power_limit: device.power_management_limit().ok(),
+            gpu_name: device.name().map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get GPU name' : {e}")).ok(),
+
+            gpu_clock_graphic: device.clock(Clock::Graphics, ClockId::Current)
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get graphic clock frequency' : {e}"))
+                .ok(),
+            gpu_clock_memory: device.clock(Clock::Memory, ClockId::Current)
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get graphic clock frequency' : {e}"))
+                .ok(),
+            gpu_clock_sm: device.clock(Clock::SM, ClockId::Current)
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get streaming multiprocessor clock frequency' : {e}"))
+                .ok(),
+            gpu_clock_video: device.clock(Clock::Video, ClockId::Current)
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get video clock frequency' : {e}"))
+                .ok(),
+
+            gpu_usage: device.utilization_rates()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get gpu usage' : {e}"))
+                .ok()
+                .map(|u: Utilization| u.gpu),
+
+            gpu_fan_speed: (0..device.num_fans()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get fan number or fan speed' : {e}"))
+                .unwrap_or(0))
+                .map(|i: u32| device.fan_speed(i)
+                .ok())
+            .collect(),
+
+            gpu_temperature: device.temperature(TemperatureSensor::Gpu).map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get temperature(s)' : {e}")).ok(),
+
+            gpu_energy_consumption: device.total_energy_consumption().map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get energy consumption' : {e}")).ok(),
+            gpu_power_consumption: power,
+            gpu_power_usage: Some((power.unwrap() as f32 / limit.unwrap() as f32) * 100.0),
 
             gpu_memory_free: device
                 .memory_info()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get GPU free memory' : {e}"))
                 .ok()
                 .map(|m: MemoryInfo| m.free as f32 / 1e9),
             gpu_memory_stat: device
                 .utilization_rates()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get GPU memory usage' : {e}"))
                 .ok()
                 .map(|u: Utilization| u.memory),
             gpu_memory_total: device
                 .memory_info()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get GPU total memory' : {e}"))
                 .ok()
                 .map(|m: MemoryInfo| m.total as f32 / 1e9),
             gpu_memory_usage: device
                 .memory_info()
+                .map_err(|e: NvmlError| error!("[{HEADER}] Data 'Failed to get GPU memory usage' : {e}"))
                 .ok()
                 .map(|m: MemoryInfo| m.used as f32 / 1e9),
 
@@ -168,7 +210,12 @@ fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
             process_sm: None,
         };
 
-        if let Ok(utilization) = device.process_utilization_stats(None) {
+        if let Ok(utilization) = device
+            .process_utilization_stats(None)
+            .map_err(|e: NvmlError| {
+                error!("[{HEADER}] Data 'Failed to get process utilization' : {e}")
+            })
+        {
             data.process_dec = Some(
                 utilization
                     .iter()
@@ -199,36 +246,6 @@ fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
                     .map(|p: &ProcessUtilizationSample| p.sm_util)
                     .sum(),
             );
-
-            if data.process_dec.is_none()
-                || data.process_enc.is_none()
-                || data.process_mem.is_none()
-                || data.process_pid.is_none()
-                || data.process_sm.is_none()
-            {
-                error!("[{HEADER}] Data 'Failed to get process utilization stats for GPU {index}'");
-            }
-        } else {
-            error!("[{HEADER}] Data 'Failed to get process utilization stats for GPU {index}'");
-        }
-
-        if data.gpu_name.is_none() {
-            error!("[{HEADER}] Data 'Failed to get name for GPU {index}'");
-        }
-        if data.gpu_usage.is_none() {
-            error!("[{HEADER}] Data 'Failed to get utilization rate for GPU {index}");
-        }
-        if data.gpu_temperature.is_none() {
-            error!("[{HEADER}] Data 'Failed to get temperature for GPU {index}'");
-        }
-        if data.gpu_power_consumption.is_none() {
-            error!("[{HEADER}] Data 'Failed to get power consumption for GPU {index}'");
-        }
-        if data.gpu_memory_total.is_none()
-            || data.gpu_memory_usage.is_none()
-            || data.gpu_memory_free.is_none()
-        {
-            error!("[{HEADER}] Data 'Failed to get memory info for GPU {index}'");
         }
 
         result.push(data);
@@ -238,7 +255,7 @@ fn collect_gpu_data() -> Result<Vec<GpuInfo>, Box<dyn Error>> {
 }
 
 /// Public function used to send JSON formatted values,
-/// from `collect_gpu_data` function result.
+/// from [`collect_gpu_data`] function result.
 pub fn get_gpu_info() {
     let data = || -> Result<Value, Box<dyn Error>> {
         let values: Vec<Value> = collect_gpu_data()?
